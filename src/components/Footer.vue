6ff862d49592b147dfc69bb2ce4c858d3e502435
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-01-19 01:07:10 +0900

    [fix] 修正

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,13 +4,16 @@
       <a href="https://.pngtree.com/so/手作りの黄金の星のベクトル"
         >手作りの黄金の星のベクトル pngから .pngtree.com/</a
       >
+      <div>デザイン：鈴木乖離</div>
+      <div>絵：鈴木乖離</div>
+      <div class="c">©2022 simo</div>
     </div>
   </div>
 </template>
 <style scoped>
 .footer {
   width: 100%;
-  height: 80px;
+  height: 200px;
   display: flex;
   align-items: center;
   background-color: blueviolet;
@@ -18,9 +21,21 @@
 .content-wrapper {
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  flex-direction: column;
   border-bottom: 3px double rgb(255, 255, 255);
   width: 100%;
   height: 80%;
+  font-size: min(4vw, 20px);
+  color: black;
+}
+.content-wrapper a {
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+  font-size: min(2vw, 10px);
+  margin: 20px 0;
+}
+.content-wrapper .c {
+  margin: 10px 0;
 }
 </style>
